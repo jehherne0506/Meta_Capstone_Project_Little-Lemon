c401dc94214f152504271595c0bfeff8f8828bff
jehherne <yeohjehherne@gmail.com>
2024-09-18 21:34:18 +0800

    Delete Little Lemon Capstone Project directory

--- Little Lemon Capstone Project/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import Reservation from './Components/Pages/Reservations/reservation';
-
-test('renders Booking Form', () => {
-  render(<Reservation />);
-  const heading_element = screen.getByText('Table Reservation')
-  expect(heading_element).toBeInTheDocument();
-});
